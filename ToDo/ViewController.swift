b13ba81f836956c685bea0b16b1597a26f01298f
jed1master <jedimaster108@gmail.com>
2023-05-29 15:42:44 +0300

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Александр Корепанов on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
