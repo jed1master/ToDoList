86aafa751ac0888ead5c701c66ce832d276d3847
jed1master <jedimaster108@gmail.com>
2023-07-28 00:02:55 +0300

    replacing the image with a button for a checkmark

--- ToDo/Model/ToDoCell.swift
+++ ToDo/Model/ToDoCell.swift
@@ -10,16 +10,20 @@ import UIKit
 class ToDoCell: UITableViewCell {
     
     var todoTitleLabel = UILabel()
-    var todoCheckImage = UIImageView()
+//    var todoCheckImage = UIImageView()
+    var todoCheckButton = UIButton()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addSubview(todoTitleLabel)
-        addSubview(todoCheckImage)
+//        addSubview(todoCheckImage)
+        addSubview(todoCheckButton)
         configureTitleLabel()
         setTitleLabelConstraints()
-        configureImageView()
-        setImageViewConstraints()
+        configureButton()
+        setButtonConstraints()
+//        configureImageView()
+//        setImageViewConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -28,14 +32,19 @@ class ToDoCell: UITableViewCell {
     
     func set(item: ToDoItem ) {
         todoTitleLabel.text = item.name
-//        todoCheckImage.image = функция которая проверяет какое значение у isDone, если false то uncheck, если true то check
-        todoCheckImage.image = UIImage(systemName: checkOrNot(isCheck: item.isDone))
+        //        todoCheckImage.image = функция которая проверяет какое значение у isDone, если false то uncheck, если true то check
+        //        todoCheckImage.image = UIImage(systemName: checkOrNot(isCheck: item.isDone))
+        todoCheckButton.setImage(UIImage(systemName: checkOrNot(isCheck: item.isDone)), for: .normal)
     }
     
-    func configureImageView(){
-        todoCheckImage.contentMode = .scaleAspectFit
-        todoCheckImage.clipsToBounds = true
-        todoCheckImage.tintColor = .black
+//    func configureImageView(){
+//        todoCheckImage.contentMode = .scaleAspectFit
+//        todoCheckImage.clipsToBounds = true
+//        todoCheckImage.tintColor = .black
+//    }
+    
+    func configureButton() {
+        todoCheckButton.setTitle("", for: .normal)
     }
     
     func configureTitleLabel() {
@@ -50,13 +59,21 @@ class ToDoCell: UITableViewCell {
         todoTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -100).isActive = true
     }
     
-    func setImageViewConstraints() {
-        todoCheckImage.translatesAutoresizingMaskIntoConstraints = false
-        todoCheckImage.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        todoCheckImage.leadingAnchor.constraint(equalTo: todoTitleLabel.trailingAnchor, constant: 50).isActive = true
-        todoCheckImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        todoCheckImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+    func setButtonConstraints() {
+        todoCheckButton.translatesAutoresizingMaskIntoConstraints = false
+        todoCheckButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        todoCheckButton.leadingAnchor.constraint(equalTo: todoTitleLabel.trailingAnchor, constant: 50).isActive = true
+        todoCheckButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        todoCheckButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+        
     }
+//    func setImageViewConstraints() {
+//        todoCheckImage.translatesAutoresizingMaskIntoConstraints = false
+//        todoCheckImage.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+//        todoCheckImage.leadingAnchor.constraint(equalTo: todoTitleLabel.trailingAnchor, constant: 50).isActive = true
+//        todoCheckImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//        todoCheckImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+//    }
     
     func checkOrNot(isCheck: Bool) -> String {
         if isCheck {
